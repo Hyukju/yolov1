eff90c0159ab1a5e1d16659da87de98fef3bd354
joo <hyukju1223@gmail.com>
2022-03-23 12:29:46 +0900

    update

--- utils.py
+++ utils.py
@@ -49,12 +49,10 @@ def do_nms(bbox_info_list, iou_th):
 
     for i in range(total):
         bbox_info_i = bbox_info_list[i]
-        print('bbox_info_i', i, bbox_info_i)
         discard = False
 
         for j in range(i + 1, total):           
             bbox_info_j = bbox_info_list[j]
-            print('bbox_info_j', j, bbox_info_j)
 
             if do_iou(bbox_info_i, bbox_info_j) > iou_th:
                 if bbox_info_j['confidence'] > bbox_info_i['confidence']:
@@ -84,3 +82,41 @@ def draw_rectangle(img, bbox_info_list):
         cv2.putText(out, f'{cl}:{confidence:.2f}', (pt1[0], pt1[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255),1)
 
     return out 
+
+def get_bbox_info_list_from_predict_single_image(predict_result, labels=None):
+    # 이미지 하나에 대한 결과만 도출 함 
+
+    features = predict_result
+    bbox_info_list = []
+    
+    for j in range(2):
+
+        if j == 0:
+            confidence = features[...,4]
+            bbox = features[...,0:4]
+        else:
+            confidence = features[...,9]
+            bbox = features[...,5:9]
+    
+        cl_list = features[...,10:]         
+    
+        for m in range(7):
+            for n in range(7):
+                if confidence[m,n] > 0.5:
+                    bbox_info = dict()
+                    # labels 가 없으면 숫자로 표시
+                    if labels == None:
+                        bbox_info['class'] = np.argmax(cl_list[m,n])
+                    else:
+                        bbox_info['class'] = labels[np.argmax(cl_list[m,n])]
+                    # yolo feature 결과는 각 m,n을 기준으로 0 ~ 1 사이로 표시되어 있음 
+                    # 이 값을 0 ~ 7 값의 범위로 변환 (yolov1 feature 7x7)
+                    bbox_info['x'] = (bbox[m,n,0] + n) /7.
+                    bbox_info['y'] = (bbox[m,n,1] + m) /7.
+                    bbox_info['width'] = bbox[m,n,2] 
+                    bbox_info['height'] = bbox[m,n,3] 
+                    bbox_info['confidence'] = confidence[m,n]
+                    bbox_info_list.append(bbox_info)
+
+    return bbox_info_list
+
